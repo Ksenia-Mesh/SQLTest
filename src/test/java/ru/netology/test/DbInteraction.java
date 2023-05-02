48a894d2e706e83e9a4bb978e7e150c6d8c31651
ksenia-mesh <ksusha545-2011@mail.ru>
2023-05-02 20:28:22 +0300

    commit

---
+++ src/test/java/ru/netology/test/DbInteraction.java
@@ -0,0 +1,93 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SQLHelper.deleteAllDB;
+import static ru.netology.data.SQLHelper.deleteCodes;
+import static ru.netology.data.DataHelper.*;
+
+public class DbInteraction {
+    @BeforeAll
+    static void setUpAll() {
+        Configuration.headless = false;
+    }
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @AfterEach
+    void setDown() {
+        deleteCodes();
+    }
+
+    @AfterAll
+    static void setDownAll() {
+        deleteAllDB();
+    }
+
+    @Test
+    void shouldReturnAccessWithFirstValidLogin() {
+        new LoginPage().validLogin(getFirstAuthInfo()).accessPage();
+    }
+
+    @Test
+    void shouldReturnAccessWithSecondValidLogin() {
+        new LoginPage().validLogin(getSecondAuthInfo()).accessPage();
+    }
+
+    @Test
+    void shouldReturnFailWithInvalidLogin() {
+        new LoginPage().invalidLogin(getInvalidAuthInfo());
+    }
+
+    @Test
+    void shouldReturnFailWithEmptyLogin() {
+        new LoginPage().emptyLoginOrPass(getEmptyLogin());
+    }
+
+    @Test
+    void shouldReturnFailWithEmptyPass() {
+        new LoginPage().emptyLoginOrPass(getEmptyPass());
+    }
+
+    @Test
+    void shouldReturnFailWithEmptyLoginAndPass() {
+        new LoginPage().emptyLoginOrPass(getEmptyAuthInfo());
+    }
+
+    @Test
+    void shouldReturnFailWithInvalidCode() {
+        new LoginPage().validLogin(getFirstAuthInfo()).invalidCode();
+    }
+
+    @Test
+    void shouldReturnFailWithEmptyCode() {
+        new LoginPage().validLogin(getSecondAuthInfo()).emptyCode();
+    }
+
+    @Test
+    void shouldEnterAccountPageFirstAccount() {
+        new LoginPage().validLogin(getFirstAuthInfo()).validCode();
+        new DashboardPage().accessLogin();
+    }
+
+    @Test
+    void shouldEnterAccountPageSecondAccount() {
+        new LoginPage().validLogin(getSecondAuthInfo()).validCode();
+        new DashboardPage().accessLogin();
+    }
+
+    @Test
+    void shouldReturnFailAfterTripleEntry() {
+        new LoginPage().validLogin(getInvalidPassFirstAuthInfo());
+        new LoginPage().validLogin(getInvalidPassFirstAuthInfo());
+        new LoginPage().validLogin(getInvalidPassFirstAuthInfo());
+        new LoginPage().invalidLogin(getFirstAuthInfo());
+    }
+}

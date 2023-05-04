1b7bc87026d4d47955da392e85031ce42009e02f
Ksenia <97454789+Ksenia-Mesh@users.noreply.github.com>
2023-05-04 21:38:45 +0300

    Update SQLHelper.java

--- src/test/java/ru/netology/data/SQLHelper.java
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -5,44 +5,38 @@ import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-import static java.sql.DriverManager.getConnection;
 
 public class SQLHelper {
-    private final static QueryRunner queryRunner = new QueryRunner();
-    private final static Connection conn = connection();
-    private static final String url = "jdbc:mysql://localhost:3306/database";
-    private static final String user = "app";
-    private static final String password = "pass";
-    static String code;
+    private static QueryRunner runner = new QueryRunner();
 
     private SQLHelper() {
+
     }
 
-    @SneakyThrows
-    private static Connection connection() {
-        return getConnection(url, user, password);
+    private static Connection getConn() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/database", "app", "pass");
     }
 
-    @SneakyThrows
-    public static String getVerifyCode() {
-        code = queryRunner.query(conn,
-                "SELECT ac.code, ac.created, u.id, ac.user_id FROM auth_codes ac, users u WHERE u.id = ac.user_id ORDER BY created DESC",
-                new ScalarHandler<>());
-        return code;
-    }
-
-
-    @SneakyThrows
-    public static void deleteCodes() {
-        queryRunner.update(conn, "DELETE FROM auth_codes");
+    public static DataHelper.VerificationCode getVerificationCode() {
+        var codeSQL = "SELECT code FROM auth_codes ORDER BY created DESC LIMIT 1";
+        try (var conn = getConn()) {
+            var code = runner.query(conn, codeSQL, new ScalarHandler<String>());
+            return new DataHelper.VerificationCode(code);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return null;
     }
 
     @SneakyThrows
     public static void deleteAllDB() {
-        queryRunner.update(conn, "DELETE FROM auth_codes");
-        queryRunner.update(conn, "DELETE FROM card_transactions");
-        queryRunner.update(conn, "DELETE FROM cards");
-        queryRunner.update(conn, "DELETE FROM users");
+        var connection = getConn();
+        runner.execute(connection, "DELETE FROM auth_codes");
+        runner.execute(connection, "DELETE FROM card_transactions");
+        runner.execute(connection, "DELETE FROM cards");
+        runner.execute(connection, "DELETE FROM users");
     }
 }
